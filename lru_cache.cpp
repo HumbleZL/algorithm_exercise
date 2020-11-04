5bf4417e0242d116b88dfa72ce15f124b896f9dc
Humble Chak <iuseitforwork@outlook.com>
2020-11-04 15:16:08 +0800

    lru_cache, cpp版修正)

--- lru_cache.cpp
+++ lru_cache.cpp
@@ -8,29 +8,22 @@ struct Node
     int key;
     int val;
     Node *prev, *next;
-    Node(int _key, int _value){
-       key = _key; 
-       val = _value;
-       prev = NULL;
-       next = NULL;
-    }
-}
+    Node(int _key, int _value):key(_key), val(_value), prev(nullptr), next(nullptr){}
+};
 
-class DoubleList()
+class DoubleList
 {
-private;
-    Node * head;
-    Node * tail;
-
-    DoubleList() {
-        head = nullptr; 
-        tail = nullptr;
-    }
+private:
+	int _size;
 
 public:
+    Node * head;
+    Node * tail;
+    DoubleList():_size(0), head(nullptr), tail(nullptr) {}
+
     int addFirst(Node * node)
     {
-        if(node == null_ptr) return -1;
+        if(node == nullptr) return -1;
 
         if(head == nullptr)
         {
@@ -39,10 +32,12 @@ public:
         }
         else
         {
-            node->next = data;
+            node->next = head;
             head->prev = node;
             head = node;
         }
+
+		_size++;
 
         return 1;
     }
@@ -67,6 +62,9 @@ public:
         delete node; //释放指针
         node = nullptr;
 
+		_size--; 
+
+		return 1;
     }
 
     int removeLast()
@@ -86,35 +84,39 @@ public:
             tail = node->prev;
         }
 
+		_size--;
+
         delete node;
         node = nullptr;
+
+		return 1;
     }
-}
+
+	int size()
+	{
+		return _size;
+	}
+};
 
 class LRUCache
 {
 private:
-    int cache_size;
     int capacity;
-    map<int, Node *> hash_map;
+    map<int, Node *>  hash_map;
     DoubleList data;
 
 public:
 
     LRUCache(int cap)
     {
-        cache_size = 0;
         capacity = cap;
-        hash_map = new map<int, Node*>();
-        data = new DoubleList();
     }
 
-    int size()
-    {
-        return cache_size;
-    }
-    
-    int capacity()
+	~LRUCache()
+	{
+	}
+
+    int get_capacity()
     {
         return capacity;
     }
@@ -128,16 +130,45 @@ public:
         
         data.remove(node);
 
-        Node * new_node = Node(key, val);
+        Node * new_node = new Node(key, val);
         data.addFirst(new_node);
         hash_map[key] = new_node;
+
+		return val;
     }
 
     int put(int key, int value)
     {
+		if(data.size() == capacity)
+		{
+			int end_key = data.tail->key;
+			data.removeLast();
+			hash_map.erase(end_key);
+		}
+
         if(hash_map.find(key) != hash_map.end())
         {
-             
+			hash_map.erase(key);
         }
+		
+		Node * node = new Node(key, value);
+		data.addFirst(node);
+		hash_map.insert({key, node});
+		return 1;
     }
+};
+
+int main()
+{
+	LRUCache cache = LRUCache( 2 /* 缓存容量 */ );
+
+	cout << cache.put(1, 1) << endl;
+	cout << cache.put(2, 2) << endl;
+	cout << cache.get(1) << endl;       // 返回  1
+	cout << cache.put(3, 3) << endl;    // 该操作会使得关键字 2 作废
+	cout << cache.get(2) << endl;       // 返回 -1 (未找到)
+	cout << cache.put(4, 4) << endl;    // 该操作会使得关键字 1 作废
+	cout << cache.get(1) << endl;       // 返回 -1 (未找到)
+	cout << cache.get(3) << endl;       // 返回  3
+	cout << cache.get(4) << endl;       // 返回  4
 }
